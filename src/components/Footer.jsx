f795f06facdfc6a0d103941b00ae61b0fd6f8b45
Guilherme Nono <guilherme.nono45789@gmail.com>
2022-11-25 17:25:48 -0300

    Updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <h3>Escreva sobre oque você tem interesse!</h3>
+        <h3></h3>
         <p>Mini Blog &copy; 2022</p>
     </footer>
   )
