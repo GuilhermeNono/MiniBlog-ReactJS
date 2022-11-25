6bfbfe015048415ab0bbedafc59f3242644a10a0
Guilherme Nono <guilherme.nono45789@gmail.com>
2022-11-25 17:35:16 -0300

    Finishing project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
       <form className={styles.search_form} onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Ou busque por tags..."
+          placeholder="Busque por tags..."
           onChange={(e) => setQuery(e.target.value)}
           value={query}
         />
