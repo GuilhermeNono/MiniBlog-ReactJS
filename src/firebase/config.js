4c115d60069a99efd9ef80138247c9fe6eb00731
Guilherme Nono <guilherme.nono45789@gmail.com>
2022-11-25 16:57:38 -0300

    Firebase config File

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "authDomain",
-  projectId: "projectId",
-  storageBucket: "storageBucket",
-  messagingSenderId: "messagingSenderId",
-  appId: "appId"
+  apiKey: `${import.meta.env.VITE_APIKEY}`,
+  authDomain: `${import.meta.env.VITE_AUTHDOMAIN}`,
+  projectId: `${import.meta.env.VITE_PROJECTID}`,
+  storageBucket: `${import.meta.env.VITE_STORAGEBUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_MESSAGINGSENDERID}`,
+  appId: `${import.meta.env.VITE_APPID}`
 };
 
 // Initialize Firebase
